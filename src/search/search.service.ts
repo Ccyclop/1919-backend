3bd166da95b1b1e24422a2ba69578d8cba3630cc
MilkyWayy18 <157787818+MilkyWayy18@users.noreply.github.com>
2024-07-16 12:59:56 +0400

    fix promise

--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -3,6 +3,9 @@ import { MusicsRepository } from 'src/musics/musics.repository';
 import { AuthorRepository } from 'src/authors/authors.repository';
 import { AlbumRepository } from 'src/album/album.repository';
 import { SearchResultDto } from './dtos/search.dto';
+import { Music } from 'src/musics/entities/music.entity';
+import { Author } from 'src/authors/entities/author.entity';
+import { Album } from 'src/album/entities/album.entity';
 
 @Injectable()
 export class SearchService {
@@ -12,14 +15,14 @@ export class SearchService {
     private readonly albumRepository: AlbumRepository
   ) {}
 
-  async searchInfo(searchString: string): Promise<{ music: any[], artist: any[], album: any[] }> {
+  async searchInfo(searchString: string): Promise<{ music: Music[], author: Author[], album: Album[] }> {
     const musicResults = await this.musicRepository.searchMusic(searchString);
     const artistResults = await this.authorRepository.searchAuthors(searchString);
     const albumResults = await this.albumRepository.searchAlbums(searchString);
 
     return {
       music: musicResults,
-      artist: artistResults,
+      author: artistResults,
       album: albumResults,
     };
   }
