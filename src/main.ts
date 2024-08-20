ddf23423405909d50ab5239e95cd4b7baa6986b3
MilkyWayy18 <157787818+MilkyWayy18@users.noreply.github.com>
2024-08-20 13:44:40 +0400

    cors 2

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
 
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
