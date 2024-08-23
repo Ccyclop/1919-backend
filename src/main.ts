1a23eb6afb03a4c317a4e8312ea6f821ef56acb3
MilkyWayy18 <157787818+MilkyWayy18@users.noreply.github.com>
2024-08-23 12:35:04 +0400

    test

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   // app.use(cookieParser());
 
   app.enableCors({
-    origin: 'http://localhost:3000', 
+    origin: ['http://localhost:3000', 'http://localhost:3001'], 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
